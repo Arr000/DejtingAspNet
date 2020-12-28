1ccbed0049725d831b5eaff15d8e42e77627fcf0
arash <arash_shahinzadeh@hotmail.com>
2020-12-29 00:42:40 +0100

    fix

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -18,7 +18,7 @@ namespace Identity
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container. 3
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppIdentityDbContext>(options =>
